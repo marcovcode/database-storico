d610041cc6284b97ee69c0ebaa576af03a4b6442
marcovidali2 <marcovidali6@proton.me>
2024-09-18 16:47:50 +0200

    feat(app): Implement theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#e11d48",
+                    "base-100": "#fef9c3",
+                },
+            },
+        ],
+    },
     theme: {
         extend: {
             fontFamily: {

c5d7296569658904714f01eb604f1f7f6cecbd80
marcovidali2 <marcovidali6@proton.me>
2024-09-18 15:50:50 +0200

    feat(app): Add search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useRef } from "react";
+
 const App = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        const handleKeyDown = () => {
+            if (inputRef.current) inputRef.current.focus();
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    }, []);
+
     return (
         <div className="p-4 font-playfair">
-            <h1 className="text-center text-5xl font-bold">Database Storico</h1>
+            <div className="flex h-dvh flex-col items-center justify-center gap-4">
+                <h1 className="text-5xl font-bold">Database Storico</h1>
+                <input
+                    ref={inputRef}
+                    type="text"
+                    className="input input-primary w-96"
+                    placeholder="Anno, avvenimento, parola chiave..."
+                />
+            </div>
         </div>
     );
 };

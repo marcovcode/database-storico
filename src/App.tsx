2d4c737aa71c0eb625db67706a70f1fbc4f87e4a
marcovidali2 <marcovidali6@proton.me>
2024-09-18 15:56:09 +0200

    feat(app): Fade out input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const App = () => {
     const searchRef = useRef<HTMLInputElement>(null);
+    const [searchValue, setSearchValue] = useState("");
 
     useEffect(() => {
         const handleKeyDown = () => {
@@ -15,12 +16,20 @@ const App = () => {
     return (
         <div className="p-4 font-playfair">
             <div className="flex h-dvh flex-col items-center justify-center gap-4">
-                <h1 className="text-5xl font-bold">Database Storico</h1>
+                <h1
+                    className={`text-5xl font-bold transition-opacity duration-200 ${
+                        searchValue ? "opacity-0" : "opacity-100"
+                    }`}
+                >
+                    Database Storico
+                </h1>
                 <input
                     ref={searchRef}
                     type="text"
                     className="input input-primary w-96"
                     placeholder="Anno, avvenimento, parola chiave..."
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
                 />
             </div>
         </div>
